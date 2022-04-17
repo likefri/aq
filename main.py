3e9160e8c482938943f833d6612eb74bc87323d8
Seo <likefri@gmail.com>
2022-04-17 23:10:28 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+from PySide6.QtWidgets import (
+    QMainWindow, QApplication,
+    QLabel, QToolBar, QStatusBar
+)
+from PySide6.QtGui import QAction, QIcon
+from PySide6.QtCore import Qt
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.setWindowTitle("My Awesome App")
+
+app = QApplication(sys.argv)
+w = MainWindow()
+w.show()
+app.exec_()
